4fc96efd9323f68aa60b1f6b28fc92a62ae50d75
Ronald Weidner <xecronix@yahoo.com>
2018-05-10 15:21:13 -0400

    Added tests for multiple sequential templates and looping actions.

--- SharpEagleTests/TestActionTag.cs
+++ SharpEagleTests/TestActionTag.cs
@@ -13,6 +13,8 @@ namespace SharpEagleTests
         {
             AddTest(new SimpleActionTest());
             AddTest(new NestedSubTemplateTest());
+            AddTest(new NestedSubTemplateLoopingTest());
+            AddTest(new MultipleSequentialActionTest());
         }
     }
 
@@ -92,8 +94,111 @@ Christian Yes";
             newTags.Add("Name", "Xecronix");
             newTags.Add("Country", "Unknown");
             newTags.Add("Christian", "Yes");
-            eagle.AddAction("demographics", new DemographicsAction());
             string retval = eagle.Parse(template, newTags);
+            return retval;
+        }
+    }
+
+    public class NestedSubTemplateLoopingTest : ISharpEagleTest, ITemplateAction
+    {
+        public string Run(string template, Dictionary<string, string> tags)
+        {
+            SharpEagle.Template eagle = new SharpEagle.Template();
+            eagle.AddAction("demographics", new LoopingDemographicsAction());
+            Dictionary<string, string> newTags = new Dictionary<string, string>();
+            newTags.Add("title", "This is a test for template nesting.");
+            string retval = eagle.Parse(template, newTags).Trim();
+            return retval;
+        }
+
+        public bool Test(out string results)
+        {
+            string template =
+ @"{@nested {=title:}{@demographics
+Name      {=Name:}
+Country   {=Country:}
+Christian {=Christian:}:}:}";
+
+            string expected =
+@"This is a test for template nesting.
+Name      Xecronix 1
+Country   Unknown 1
+Christian Yes 1
+Name      Xecronix 2
+Country   Unknown 2
+Christian Yes 2
+Name      Xecronix 3
+Country   Unknown 3
+Christian Yes 3";
+            SharpEagle.Template eagle = new SharpEagle.Template();
+            eagle.AddAction("nested", this);
+            Dictionary<string, string> tags = new Dictionary<string, string>();
+            results = eagle.Parse(template, tags);
+            bool retval = expected.Equals(results);
+            return retval;
+        }
+    }
+
+    public class LoopingDemographicsAction : ITemplateAction
+    {
+        public string Run(string template, Dictionary<string, string> tags)
+        {
+            string retval = "";
+            SharpEagle.Template eagle = new SharpEagle.Template();
+            
+            for (int i = 1; i < 4; i++)
+            {
+                Dictionary<string, string> newTags = new Dictionary<string, string>();
+                newTags.Add("Name", "Xecronix " + i);
+                newTags.Add("Country", "Unknown " + i);
+                newTags.Add("Christian", "Yes " + i);
+                retval += eagle.Parse(template, newTags);
+            }
+            return retval;
+        }
+    }
+
+    public class MultipleSequentialActionTest : ISharpEagleTest, ITemplateAction
+    {
+        public string Run(string template, Dictionary<string, string> tags)
+        {
+            SharpEagle.Template eagle = new SharpEagle.Template();
+            Dictionary<string, string> newTags = new Dictionary<string, string>();
+            newTags.Add("title", "Multiple Sequential Action Test.");
+            string retval = eagle.Parse(template, newTags).Trim();
+            return retval;
+        }
+
+        public bool Test(out string results)
+        {
+            string template =
+@"{@nested {=title:}:}
+{@multiaction1{=sub:}:}";
+
+            string expected =
+@"Multiple Sequential Action Test.
+MultiAction1 TEXT";
+            SharpEagle.Template eagle = new SharpEagle.Template();
+            eagle.AddAction("nested", this);
+            eagle.AddAction("multiaction1", new MultiAction1());
+            Dictionary<string, string> tags = new Dictionary<string, string>();
+            results = eagle.Parse(template, tags);
+            bool retval = expected.Equals(results);
+            return retval;
+        }
+    }
+
+    public class MultiAction1 : ITemplateAction
+    {
+        public string Run(string template, Dictionary<string, string> tags)
+        {
+            string retval = "";
+            SharpEagle.Template eagle = new SharpEagle.Template();
+
+            Dictionary<string, string> newTags = new Dictionary<string, string>();
+            newTags.Add("sub", "MultiAction1 TEXT");
+            retval += eagle.Parse(template, newTags);
+
             return retval;
         }
     }
